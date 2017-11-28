6005d67a2cdac4d6e65b5f265b3a069a352e3a10
yulong <844946414@qq.com>
2017-11-28 20:16:01 +0800

    particular course

--- Java/Hanming.java
+++ Java/Hanming.java
@@ -5,15 +5,16 @@ public class Hanming {
 		System.out.println(Hanming1(11,22));
 		System.out.println(Hanming2(1,4));
 	}
-	//解法1
+	//陆芒路篓1
 	public static int Hanming1(int x,int y){
 		int vor = x^y, count = 0;
 		for(int i=0; i<32;i++) count+=(vor>>i)&1;
 		return count;
 	}
-	//解法2 
-	//Integer.bitCount()函数的源代码很有意思，下面是函数的详解
-	//http://blog.csdn.net/cor_twi/article/details/53720640 
+	//陆芒路篓2 
+	//Integer.bitCount()潞炉脢媒碌脛脭麓麓煤脗毛潞脺脫脨脪芒脣录拢卢脧脗脙忙脢脟潞炉脢媒碌脛脧锚陆芒
+	//娣卞叆瑙ｈbitCount()
+	//http://blog.csdn.net/qq_33516547/article/details/78156879
 	public static int Hanming2(int x, int y){
 		return Integer.bitCount(x^y);
 	}
