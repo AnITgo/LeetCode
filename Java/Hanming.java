50ff8eb1d10f3ed21ee47305b2ceadbd145a59e9
yulong <844946414@qq.com>
2017-11-28 20:17:19 +0800

    detail

--- Java/Hanming.java
+++ Java/Hanming.java
@@ -5,15 +5,14 @@ public class Hanming {
 		System.out.println(Hanming1(11,22));
 		System.out.println(Hanming2(1,4));
 	}
-	//½â·¨1
+	//case 1
 	public static int Hanming1(int x,int y){
 		int vor = x^y, count = 0;
 		for(int i=0; i<32;i++) count+=(vor>>i)&1;
 		return count;
 	}
-	//½â·¨2 
-	//Integer.bitCount()º¯ÊýµÄÔ´´úÂëºÜÓÐÒâË¼£¬ÏÂÃæÊÇº¯ÊýµÄÏê½â
-	//深入解读bitCount()
+	//case 2 
+	// bitCount()
 	//http://blog.csdn.net/qq_33516547/article/details/78156879
 	public static int Hanming2(int x, int y){
 		return Integer.bitCount(x^y);
