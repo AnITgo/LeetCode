f6db065d306f085f2cc3b654982b84e23dc4f124
yulong <844946414@qq.com>
2017-12-12 09:06:55 +0800

    2017/12/12 快排与双轴快排

--- Java/ArrayPairSum.java
+++ Java/ArrayPairSum.java
@@ -1,6 +1,18 @@
-
+/*
+561. Array Partition I
+	Given an array of 2n integers, your task is to group these integers into n pairs of integer, 
+	say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+Example:
+	Input: [1,4,3,2]
+	Output: 4
+	Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
+Note:
+	n is a positive integer, which is in the range of [1, 10000].
+	All the integers in the array will be in the range of [-10000, 10000].
+*/
 public class ArrayPairSum {
 	public static int arrayPairSum(int[] nums){
+	//冒泡排序，输入过大时，运行超时，算法时间开销大
 		for(int j = 0;j< nums.length;j++){
 			for(int i = 0;i<nums.length-j-1; i++){
 				if(nums[i]>nums[i+1]){
@@ -10,6 +22,8 @@ public class ArrayPairSum {
 				}
 			}
 		}
+	//.sort 源码探究  排序算法优化
+		Arrays.sort(nums);
 		
 		int output = 0;
 		for (int i = 0; i < nums.length-1; i++) {
